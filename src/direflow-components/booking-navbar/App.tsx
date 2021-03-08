0445c3fa4c4ea3d9ebc38caeb9ef0ce412c55fd5
Roberto Vázquez González <robertovg@gmail.com>
2021-03-08 22:08:59 +0100

    Add basic working example for react-dates

--- src/direflow-components/booking-navbar/App.tsx
+++ src/direflow-components/booking-navbar/App.tsx
@@ -1,6 +1,13 @@
-import React, { FC, useContext } from "react";
+import React, { FC, useContext, useState } from "react";
 import { EventContext } from "direflow-component";
+import { Styled } from "direflow-component";
+
 import styled from "styled-components";
+import { DateRangePicker } from "react-dates";
+import "react-dates/initialize";
+import dateCss from "react-dates/lib/css/_datepicker.css";
+
+import moment, { Moment } from "moment";
 
 interface IProps {
   bookingURL?: string;
@@ -20,6 +27,9 @@ const AppStyled = styled.div`
 const App: FC<IProps> = ({ bookingURL = "https://bookingURL" }) => {
   const dispatch = useContext(EventContext);
 
+  const [startDate, setStartDate] = useState<Moment | null>(moment());
+  const [endDate, setEndDate] = useState<Moment | null>(moment());
+
   const handleClick = () => {
     window.open(bookingURL);
     const event = new Event("my-event");
@@ -27,12 +37,29 @@ const App: FC<IProps> = ({ bookingURL = "https://bookingURL" }) => {
   };
 
   return (
-    <AppStyled>
-      <div>Works</div>
-      <button className="button" onClick={handleClick}>
-        Click me!
-      </button>
-    </AppStyled>
+    <Styled styles={dateCss}>
+      <AppStyled>
+        <DateRangePicker
+          startDate={startDate} // momentPropTypes.momentObj or null,
+          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+          endDate={endDate} // momentPropTypes.momentObj or null,
+          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+          onDatesChange={({
+            startDate: startDateParam,
+            endDate: endDateParam,
+          }) => {
+            setStartDate(startDateParam);
+            setEndDate(endDateParam);
+          }} // PropTypes.func.isRequired,
+          focusedInput={null} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+          onFocusChange={() => null}
+          minimumNights={1}
+        />
+        <button className="button" onClick={handleClick}>
+          Click me!
+        </button>
+      </AppStyled>
+    </Styled>
   );
 };
 
