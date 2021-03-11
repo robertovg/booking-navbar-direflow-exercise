34e2878d457eba5fdd12e1b6ccf03d3b448870f1
Roberto Vázquez González <robertovg@gmail.com>
2021-03-12 00:23:45 +0100

    Add styles and basic behaviour

--- src/direflow-components/booking-navbar/App.tsx
+++ src/direflow-components/booking-navbar/App.tsx
@@ -1,65 +1,99 @@
-import React, { FC, useContext, useState } from "react";
-import { EventContext } from "direflow-component";
-import { Styled } from "direflow-component";
-
-import styled from "styled-components";
-import { DateRangePicker } from "react-dates";
-import "react-dates/initialize";
-import dateCss from "react-dates/lib/css/_datepicker.css";
-
 import moment, { Moment } from "moment";
+import React, { useState } from "react";
+import styled from "styled-components";
+import DatePicker from "./DatePicker";
 
-interface IProps {
+interface AppProps {
   bookingURL?: string;
+  lightAccent?: string;
+  strongAccent?: string;
+  disabledAccent?: string;
+  currency?: string;
+  bookingActionLabel?: string;
 }
+
+const orangeLight = "#f2937c";
+const orangeStrong = "#f87459";
+const brownLight = "#bcafa2";
 
 const AppStyled = styled.div`
   width: 100%;
   height: auto;
   display: grid;
   grid-auto-flow: column;
+  column-gap: 1rem;
   &,
   & * {
     font-family: "Lato", sans-serif;
   }
+  & > button {
+    transition: background-color 0.3s ease;
+
+    & {
+      font-size: 16px;
+      text-transform: uppercase;
+      padding: 15px 30px;
+      border: none;
+      cursor: pointer;
+      justify-content: space-around;
+      background: ${orangeStrong};
+      color: white;
+    }
+    &:hover {
+      background: ${orangeLight};
+      color: white;
+    }
+  }
 `;
 
-const App: FC<IProps> = ({ bookingURL = "https://bookingURL" }) => {
-  const dispatch = useContext(EventContext);
-
+const App = ({
+  bookingURL = "https://bookingURL",
+  lightAccent = orangeLight,
+  strongAccent = orangeStrong,
+  disabledAccent = brownLight,
+  currency = "EUR",
+  bookingActionLabel = "book",
+}: AppProps): JSX.Element => {
   const [startDate, setStartDate] = useState<Moment | null>(moment());
   const [endDate, setEndDate] = useState<Moment | null>(moment());
 
-  const handleClick = () => {
-    window.open(bookingURL);
-    const event = new Event("my-event");
-    dispatch(event);
+  const handleDatesChange = ({
+    startDate,
+    endDate,
+  }: {
+    startDate: Moment | null;
+    endDate: Moment | null;
+  }) => {
+    setStartDate(startDate);
+    setEndDate(endDate);
+  };
+
+  const openBookingPage = () => {
+    const params = [
+      ...(startDate ? [`checkInDate=${startDate?.format("YYYY-MM-DD")}`] : []),
+      ...(endDate ? [`checkOutDate=${endDate?.format("YYYY-MM-DD")}`] : []),
+      `locale=${(window?.navigator?.language ?? "en").substr(0, 2)}`,
+      `currency=${currency}`,
+    ];
+
+    const urlToOpen = `${bookingURL}${params
+      .map((e, i) => `${i ? "&" : "?"}${e}`)
+      .join("")}`;
+    window.open(urlToOpen);
   };
 
   return (
-    <Styled styles={dateCss}>
-      <AppStyled>
-        <DateRangePicker
-          startDate={startDate} // momentPropTypes.momentObj or null,
-          startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-          endDate={endDate} // momentPropTypes.momentObj or null,
-          endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-          onDatesChange={({
-            startDate: startDateParam,
-            endDate: endDateParam,
-          }) => {
-            setStartDate(startDateParam);
-            setEndDate(endDateParam);
-          }} // PropTypes.func.isRequired,
-          focusedInput={null} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-          onFocusChange={() => null}
-          minimumNights={1}
-        />
-        <button className="button" onClick={handleClick}>
-          Click me!
-        </button>
-      </AppStyled>
-    </Styled>
+    <AppStyled>
+      <DatePicker
+        lightAccent={lightAccent}
+        strongAccent={strongAccent}
+        disabledAccent={disabledAccent}
+        onDateChange={handleDatesChange}
+      />
+      <button className="button" onClick={openBookingPage}>
+        {bookingActionLabel}
+      </button>
+    </AppStyled>
   );
 };
 
---
+++ src/direflow-components/booking-navbar/DatePicker.tsx
@@ -0,0 +1,146 @@
+import { Moment } from "moment";
+import React, { useState } from "react";
+import { DateRangePicker, FocusedInputShape } from "react-dates";
+import "react-dates/initialize";
+import dateCss from "react-dates/lib/css/_datepicker.css";
+import styled, { css } from "styled-components";
+
+interface DatePickerStyledProps {
+  lightAccent: string;
+  strongAccent: string;
+  disabledAccent: string;
+}
+interface DatePickerProps extends DatePickerStyledProps {
+  onDateChange?: (dateChange: {
+    startDate: Moment | null;
+    endDate: Moment | null;
+  }) => void;
+}
+
+const DatePickerStyled = styled.div<DatePickerProps>`
+  ${dateCss}
+  padding-right: 22px;
+  border: 1px solid #dbdbdb;
+  @media (max-width: 500px) {
+    .DateRangePickerInput_calendarIcon {
+      display: none;
+    }
+  }
+
+  ${(props) => {
+    const { lightAccent, strongAccent, disabledAccent } = props;
+    return css`
+      .DateRangePickerInput__withBorder {
+        border: none;
+      }
+      .CalendarDay__selected_span {
+        background: ${lightAccent};
+        border-color: ${lightAccent};
+        color: white;
+      }
+      .CalendarDay__selected {
+        background: ${strongAccent};
+        border-color: ${strongAccent};
+        color: white;
+      }
+      .CalendarDay__selected_span:hover,
+      .CalendarDay__selected:hover {
+        background: ${strongAccent};
+        border-color: ${strongAccent};
+        color: white;
+      }
+      .CalendarDay__hovered_span:hover,
+      .CalendarDay__hovered_span {
+        background: ${lightAccent};
+        border-color: ${lightAccent};
+        color: white;
+      }
+
+      .CalendarDay__default:hover {
+        background: ${strongAccent};
+        border-color: ${strongAccent};
+        color: white;
+      }
+
+      .CalendarDay__default.CalendarDay__blocked_out_of_range:hover {
+        background: ${disabledAccent};
+        border-color: ${disabledAccent};
+        color: white;
+      }
+
+      .DateInput_input__focused {
+        border-color: ${strongAccent};
+      }
+      .DayPickerKeyboardShortcuts_show__bottomRight::before {
+        border-right-color: ${lightAccent};
+        color: white;
+      }
+      .DayPickerKeyboardShortcuts_show__bottomRight:hover::before {
+        border-right-color: ${strongAccent};
+        color: white;
+      }
+    `;
+  }};
+`;
+const DatePicker = ({
+  lightAccent,
+  strongAccent,
+  disabledAccent,
+  onDateChange,
+}: DatePickerProps): JSX.Element => {
+  const [startDate, setStartDate] = useState<Moment | null>(null);
+  const [endDate, setEndDate] = useState<Moment | null>(null);
+  const [focusedInput, setFocusedInput] = useState<FocusedInputShape | null>(
+    null,
+  );
+  const handleDatesChange = ({
+    startDate,
+    endDate,
+  }: {
+    startDate: Moment | null;
+    endDate: Moment | null;
+  }) => {
+    setStartDate(startDate);
+    setEndDate(endDate);
+    if (startDate && endDate) {
+      setFocusedInput(null);
+    }
+    if (onDateChange) {
+      onDateChange({ startDate, endDate });
+    }
+  };
+  return (
+    <DatePickerStyled
+      className="App"
+      lightAccent={lightAccent}
+      strongAccent={strongAccent}
+      disabledAccent={disabledAccent}
+    >
+      <DateRangePicker
+        showClearDates={false}
+        showDefaultInputIcon={true}
+        startDate={startDate}
+        startDateId="selector-start-date"
+        endDate={endDate}
+        endDateId="selector-end-date"
+        onDatesChange={handleDatesChange}
+        focusedInput={focusedInput}
+        onFocusChange={(focusedInputParam: FocusedInputShape | null) => {
+          if (startDate && endDate) {
+            if (focusedInputParam === "startDate") {
+              setStartDate(null);
+            }
+            if (focusedInputParam === "endDate") {
+              setEndDate(null);
+            }
+          }
+          if (focusedInputParam) {
+            setFocusedInput(focusedInputParam);
+          }
+        }}
+      />
+    </DatePickerStyled>
+  );
+};
+
+export default DatePicker;
